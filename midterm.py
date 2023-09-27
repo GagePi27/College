ea7f8f04cbd1609d7c3e952cb983ff33d4f3dab4
GagePi27 <146131545+GagePi27@users.noreply.github.com>
2023-09-26 22:31:36 -0500

    Update midterm.py
    9/26/23
    10:30pm

--- midterm.py
+++ midterm.py
@@ -2,12 +2,51 @@
 Slow down typing speed if possible...
 
 """
-
 ##############################
-#IMPORTS
+#IMPORTS_A
 import os
 import sys
+import time
 
+##############################
+#ROOT CHECK (REQUIRED FOR VARIOUS ACTIONS)
+#Get User ID
+username = os.geteuid()
+print('Username ID:' + str(username))
+print()
+time.sleep(0.75)
+
+#Check if ROOT
+if username == 0:
+    print('ROOT user active!')
+
+#Request ROOT access
+else:
+    print('''*WARNING*
+This application requires ROOT access...
+Please log into ROOT now.
+          ''')
+    print('Is the root password set? (y/n)')
+    rootSet = input('> ')
+    rootSet = rootSet.upper()
+    if rootSet == 'NO' or rootSet == 'N':
+        print('Please set your ROOT Password now!')
+        os.system('passwd root')
+        print()
+        print('After getting ROOT access, enter the following command...')
+        print('     python3 ' + __file__)
+        os.system('su -')
+    elif rootSet == 'YES' or rootSet == 'Y':
+        print('After getting ROOT access, enter the following command...')
+        print('     python3 ' + __file__)
+        os.system('su -')
+
+##############################
+#PIP CHECK
+os.system('apt-get install pip')
+
+##############################
+#IMPORTS_B (NOT PRE-INSTALLED)
 try:
     import pyperclip
 except:
@@ -21,12 +60,62 @@ except:
     import pyfiglet
 
 ##############################
+#FUNCTIONS
+#Print Title Function
+def title():
+    os.system('clear')
+    print(pyfiglet.figlet_format('TERMINEX\n'))
+    print('-----' * 20)
+
+#Print Blinky Function
+def blinky(input, title):
+    n = 8
+    for i in range(n):
+        os.system('clear')
+        title()
+        if (i % 2) == 0:
+            print(input)
+            time.sleep(0.5)
+        else:
+            print()
+            time.sleep(0.5)
+
+#Print Linux Menu
+def menuLinux():
+    print('A. Update and Upgrade')
+    print('B. Net-Tools')
+    print('C. ')
+    print('D. ')
+    print('Y. Learn the Truth about this Project')
+    print('Z. Exit Program')
+
+#Print Windows Menu
+#def menuWindows():
+#    print('A. ')
+#    print('B. ')
+#    print('C. ')
+#    print('D. ')
+
+def netTools():
+    os.system('clear')
+    print('A. See ALL Information')
+    print('B. See Active Interfaces')
+    print('C. See Down Interfaces')
+    print('Z. Return to Menu!')
+
+##############################
 #WELCOME
 
+os.system('clear')
+time.sleep(0.2)
+
 print(pyfiglet.figlet_format('Welcome!!'))
+
+time.sleep(1)
+
 print('''
 Welcome to TERMINEX!
-    
+
 This program is your menu to the world of 
     Linux Terminals...
     Commmand Lines...
@@ -49,12 +138,13 @@ else:
 
 print('System OS: ' + system)
 print('-----' * 20 + '\n')
+time.sleep(2)
 
 #OS(LINUX) ACCEPTANCE
 if system == 'Linux':
     print('''CONGRATULATIONS!
 * Your OS is currently supported by TERMINEX
-* Entering TERMINEX...''')
+     Entering TERMINEX...''')
 
 #UNSUPPORTED OS
 else:
@@ -80,10 +170,48 @@ else:
             print('\nGoodbye!')
             sys.exit() #EXIT
         else:
-            print('That is not a correct answer. Try Again')   
+            print('That is not a correct answer. Try Again')
 
 ##############################
 #MENU START
 
-print(pyfiglet.figlet_format('\n\nTERMINEX'))
-print('THIS IS WHERE THE MENU GOES') #THIS IS WHERE THE MENU GOES
+time.sleep(5)
+
+while True:
+    title()
+    menuLinux()
+    print()
+    print('Please enter a letter.')
+    menu = input('> ')
+    menu = menu.upper()
+
+    if menu == 'A':
+        os.system('apt upgrade && update -y')
+        time.sleep(2)
+    elif menu == 'B':
+        while True:
+            os.system('apt-get install net-tools')
+            netTools()
+            print()
+            print('Please enter a letter.')
+            netMenu = input('> ')
+            netMenu = netMenu.upper()
+            if netMenu == 'A':
+                print('netmenu a') ####DO I CONTINUE THE NETTOOLS MENU HERE OR SHOULD IT BE PUT INSDIE THE FUNCTION
+            if netMenu == 'Z':
+                break
+    elif menu == 'Y':
+        print('''
+The truth about this project is that...
+  I wanted to make a program I would genuinely use.
+But after learning so much code........
+  I will probably never need it again.
+              ''')
+        time.sleep(7)
+    elif menu == 'Z':
+        print('Thank you for using TERMINEX! Goodbye!')
+        time.sleep(1)
+        title()
+        sys.exit()
+    else:
+        blinky('That is not Valid. Try Again.', title)
